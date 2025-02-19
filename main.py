34fab38ad48de57bee4cce2c7e912569a810a3e7
trentenfleetham <trentenfleetham@gmail.com>
2025-02-18 16:26:50 -0800

    added asteroids and collisions

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
 
@@ -20,10 +23,16 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    # creating player groups
+    # creating groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     
+    # creating containers
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
     Player.containers = (updatable, drawable)
 
     # spawn player in middle of screen
@@ -38,6 +47,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+                updatable.update(dt)
+
+        # collisions
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over!")
+                sys.exit()
+
         # Black screen
         screen.fill((0,0,0))
 
